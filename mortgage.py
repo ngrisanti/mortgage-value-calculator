dc90960f332b36e9692019a03728ceac5b061739
Nick Grisanti <ngrisanti@hmc.edu>
2024-06-10 21:01:40 -0500

    finished reworking mortgage portion

--- mortgage.py
+++ mortgage.py
@@ -27,7 +27,6 @@ def mortgageReturnAnalysis(housePrice:float, interestRateAnnual:float, downPayme
     c = 0 if l <= 0 else c
 
     paymentNoTax_monthly = l*(i*(1+i)**n)/((1+i)**n-1)
-    paymentNoTax_total = paymentNoTax_monthly * n
 
     loanRemaining_monthly = np.array([l])
     interestPayments_monthly = np.array([])
@@ -76,9 +75,12 @@ def mortgageReturnAnalysis(housePrice:float, interestRateAnnual:float, downPayme
         mortgageRateOfReturn_monthly = (houseEquity_monthly - sumOfAllPayments_monthly) / sumOfAllPayments_monthly
 
         if showPlot:
-            plt.plot(range(len(mortgageRateOfReturn_monthly)), mortgageRateOfReturn_monthly*100)
+            xVals = list(range(len(mortgageRateOfReturn_monthly)))
+            xVals = [1 + x for x in xVals]
+            plt.plot(xVals, mortgageRateOfReturn_monthly*100)
+            plt.plot([n, n], [min(mortgageRateOfReturn_monthly*100), max(mortgageRateOfReturn_monthly*100)])
             plt.xlabel('Months')
-            plt.xticks([48*year for year in range(int(analysisTimePeriodYears/4))])
+            plt.xticks([48*year for year in range(int(analysisTimePeriodYears/4)+1)])
             plt.ylabel('Return on Investment (%)')
             plt.grid()
             plt.show()
@@ -89,47 +91,43 @@ def mortgageReturnAnalysis(housePrice:float, interestRateAnnual:float, downPayme
     return_df['loan_remaining'] = loanRemaining_monthly[:-1]
     return_df['home_value'] = houseValue_monthly
     return_df['home_equity'] = houseEquity_monthly
+    return_df['all_payments_this_month'] = totalPayments_monthly
+    return_df['all_payments_previous'] = [sum(return_df['all_payments_this_month'][:month]) \
+                                                 for month in range(len(return_df['all_payments_this_month']))]
     return_df['tax_payment_this_month'] = taxPayments_monthly
-    return_df['tax_payments_cumulative'] = [sum(taxPayments_monthly[:month]) for month in range(len(taxPayments_monthly))]
+    return_df['tax_payments_previous'] = [sum(taxPayments_monthly[:month]) for month in range(len(taxPayments_monthly))]
     return_df['mortgage_payment_this_month'] = np.concatenate(([paymentNoTax_monthly for _ in range(n)], \
                                                                [0 for _ in range(n, analysisTimePeriodYears*12)]))
-    return_df['mortgage_payments_cumulative'] = [sum(return_df['mortgage_payment_this_month'][:month]) \
+    return_df['mortgage_payments_previous'] = [sum(return_df['mortgage_payment_this_month'][:month]) \
                                                  for month in range(len(return_df['mortgage_payment_this_month']))]
-    # TODO: finish building return_df
+    return_df['interest_payment_this_month'] = interestPayments_monthly[:-1]
+    return_df['interest_payments_previous'] = [sum(return_df['interest_payment_this_month'][:month]) \
+                                                 for month in range(len(return_df['interest_payment_this_month']))]
+    return_df['principal_payment_this_month'] = principalPayments_monthly
+    return_df['principal_payments_previous'] = [sum(return_df['principal_payment_this_month'][:month]) \
+                                                 for month in range(len(return_df['principal_payment_this_month']))]
+    
+    # Replace {}_previous columns with {}_cumulative columns (to include current month)
+    for col in return_df.columns:
+        col_basis = col[:-9]
+        if col[-9:] == '_previous':
+            if col_basis == 'all_payments':
+                return_df[f'{col_basis}_cumulative'] = return_df[f'{col_basis}_this_month'] + return_df[f'{col_basis}_previous']
+                return_df = return_df.drop(f'{col_basis}_previous', axis=1)
+            else:
+                col_basis = col_basis[:-1]
+                return_df[f'{col_basis}s_cumulative'] = return_df[f'{col_basis}_this_month'] + return_df[f'{col_basis}s_previous']
+                return_df = return_df.drop(f'{col_basis}s_previous', axis=1)
 
+    return_df.index += 1
     return return_df
 
-mortgageReturnAnalysis(400000, 0.04, 40000, 0.03, 15, 0.02, 0.05, showPlot=True)
-
-
-
-# def compoundInterest(time_years:float, interestRate_annual:float, monthlyContributions:float|list[float]|np.array, 
-#                      principal:float=None, compoundingFrequency:float|str=None):
-#     if contributionFrequency is None:
-#         contributionFrequency = 'monthly'
-#     if principal is None:
-#         principal = 0
-#     if compoundingFrequency is None:
-#         compoundingFrequency = 'monthly'
-
-#     compoundingMap = {'daily': 365, 'monthly': 12, 'quarterly': 4, 'semi-annually': 2, 'annually': 1}
-#     if isinstance(compoundingFrequency, str):
-#         try:
-#             numContributions_annual = compoundingMap[contributionFrequency]
-#         except:
-#             raise ValueError("`compoundingFrequency` must either be a float or one of the following strings: \
-#                              'daily', 'monthly', 'quarterly', 'semi-annually', or 'annually'.")
-#     if not (isinstance(monthlyContributions, list) or isinstance(monthlyContributions, np.array)):
-#         monthlyContributions = np.array([monthlyContributions for _ in range(12*time_years)])
-#     if len(monthlyContributions) != 12*time_years:
-#         raise ValueError("`monthlyContributions` must either be a float or an array of \
-#                          length 12*`time_years`.")
+def compoundInterestCalculator(principal:float, interestRateAnnual:float, monthlyContribution:float = None,
+                               compoundingsPerYear:float = None, analysisTimePeriodYears:float = None) -> pd.DataFrame:
     
-#     balance = principal
-
-#     if compoundingFrequency == 365:
-#         pass
-#     else:
-#         for i in range(compoundingFrequency*time_years):
-#             pass
-
+    if monthlyContribution is None:
+        monthlyContribution = 0
+    if compoundingsPerYear is None:
+        compoundingsPerYear = 12
+    if analysisTimePeriodYears is None:
+        analysisTimePeriodYears = 40
